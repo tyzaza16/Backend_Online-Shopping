70ce4eb18d4728a2996e060994a1f67447cca5b4
Sorathorn Kaewchotchuangkul <sarathorn.16@gmail.com>
2023-10-15 23:36:05 +0700

    add api - orderList of user

--- src/service/OrderListService.ts
+++ src/service/OrderListService.ts
@@ -1,33 +1,134 @@
-// import { Request, Response } from "express";
-// import { DtoResp } from "../common/model/DataObjResp";
-// import { HandlerStatus } from "../Constant";
-// import { Transaction, TransactionModel } from "../db/model/transactionModel";
+import { Request, Response } from "express";
+import { DtoResp } from "../common/model/DataObjResp";
+import { HandlerStatus } from "../Constant";
+import { Transaction, TransactionModel } from "../db/model/transactionModel";
 
 
-// interface OrderListUser extends Transaction {
-//   userOrderList: {
-//     productId: string,
-//     status: string,
-//     quantity: number,
-//     amount: number,
-//     transactionIdRef: string,
-//   }[]
-// }
+interface OrderListUser extends Transaction {
+  userOrderList: {
+    productId: string,
+    status: string,
+    quantity: number,
+    amount: number,
+    transactionIdRef: string,
+  }[]
+}
 
-// export class OrderListService {
+export class OrderListService {
 
-//   async getOrderListOfUser(email: string, res: Response): Promise<Response> {
+  async getOrderListOfUser(email: string, res: Response): Promise<Response> {
 
-//     const dtoResp: DtoResp = new DtoResp();
-//     dtoResp.setStatus(HandlerStatus.Failed);
+    const dtoResp: DtoResp = new DtoResp();
     
-//     const userOrderList: OrderListUser[] = TransactionModel.aggregate([
-//       {
-//         $match: {
-//           email: email
-//         }
-//       }
-//     ]);
-//   }
+    const userOrderList: OrderListUser[] = await TransactionModel.aggregate([
+      {
+        $match: {
+          email: email
+        }
+      },
+      {
+        $lookup: {
+          from: 'users',
+          localField: '_id',
+          foreignField: 'orderList.transactionIdRef',
+          as: 'orderDetail'
+        }
+      },
+      {
+        $unset: [
+          "orderDetail._id",
+          "orderDetail.email",
+          "orderDetail.password",
+          "orderDetail.profileName",
+          "orderDetail.address",
+          "orderDetail.role",
+          "orderDetail.likeProduct",
+          "orderDetail.cart",
+          "orderDetail.waitingPayment",
+          "orderDetail.__v",
+          "orderDetail.telNo",
+          "orderDetail.hashedCode"
+        ]
+      }, 
+      {
+        $unwind: "$orderDetail"
+      },
+      {
+        $unwind: "$orderDetail.orderList"
+      },
+      {
+        $group: {
+          _id: "$_id",
+          orderDetail: {
+            $push: {
+              $cond: [
+                {$eq: ["$orderDetail.orderList.transactionIdRef","$_id"],},
+                "$orderDetail.orderList",
+                null
+              ]
+            }
+          },
+          address:{
+            $first: "$address"
+          },
+          totalAmount:{
+            $first: "$totalAmount"
+          }
+        }  
+      },
+      {
+        $addFields: {
+          orderDetail: {
+                $filter: {
+                    input: "$orderDetail",
+                    as: "orderItem",
+                    cond: {
+                        $ne: [ "$$orderItem", null ]
+                    }
+                }
+            }
+        }
+      },
+      {
+        $unwind: "$orderDetail"
+      },
+      {
+        $lookup: {
+          from: "products",
+          localField: 'orderDetail.productId',
+          foreignField: '_id',
+          as: 'orderDetail.productDetail'
+        }
+      },
+      {
+        $unset: "orderDetail.productDetail.productImage",
+      },
+      {
+        $unwind: "$orderDetail.productDetail",
+      },
+      {
+        $group: {
+          _id: "$_id",
+          orderDetail: {
+            $push: "$orderDetail"
+          },
+          address:{
+            $first: "$address"
+          },
+          totalAmount:{
+            $first: "$totalAmount"
+          }
+        }
+      },
+      {
+        $sort: {_id: 1}
+      }
+    ]);
 
-// }
+    dtoResp.setStatus(HandlerStatus.Success);
+    dtoResp.setMessage('Successfully get order!.');
+    return res.status(200).json({ ...dtoResp, userOrderList });
+  }
+
+}
+
